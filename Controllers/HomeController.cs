ee37c4042cdf5fffc7e2b6fb02ec0cac2e9bc77e
Wesley Cowan <wncowan@gmail.com>
2018-05-10 15:51:08 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using csharp_form.Models;
+
+namespace csharp_form.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(){
+
+        }
+        // GET: /Home/
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/process")]
+        public IActionResult Process(string firstName, string lastName, int age, string email, string password)
+        {
+            User newUser = new User
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Age = age,
+                Email = email,
+                Password = password
+            };
+            
+            if (TryValidateModel(newUser) == false)
+            {
+                ViewBag.errors = ModelState.Values;
+                return View();
+            }
+
+            else
+            {
+                return RedirectToAction("Success");
+            }
+        }
+
+        [HttpGet]
+        [Route("/success")]
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ Views/Home/Success.cshtml
@@ -0,0 +1,1 @@
+<h2>You have successfully registered!</h2>
